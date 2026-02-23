641d46a19c692d6147c27d7762f9ef37a2a3e639
leeleell2027-crypto <leeleell2027@gmail.com>
2026-02-23 14:30:22 +0900

    tesd eclipse11

--- src/main/java/com/example/demo2/Demo2Application.java
+++ src/main/java/com/example/demo2/Demo2Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Demo2Application {
 
-	// tesd eclipse
+	// tesd eclipse11
 	public static void main(String[] args) {
 		SpringApplication.run(Demo2Application.class, args);
 	}
